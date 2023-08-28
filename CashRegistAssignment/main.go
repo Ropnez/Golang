a6b8c812342bbc19365f7999c9957047d057c014
Burak <100238698+Ropnez@users.noreply.github.com>
2023-08-28 16:15:30 +0300

    CashRegistAssignment

---
+++ CashRegistAssignment/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Describable interface {
+	Describe() string
+}
+
+type Item struct {
+	Name          string
+	Price         float64
+	DiscountPrice float64
+}
+
+func calcutePrice(item Item) float64 {
+	return item.Price - item.DiscountPrice
+}
+
+func totalPrice(items []Item) float64 {
+	var total float64 = 0
+	for _, item := range items {
+		total += calcutePrice(item)
+	}
+	return total
+}
+
+func main() {
+	a := Item{
+		Name:          "elma",
+		Price:         10,
+		DiscountPrice: 1.5,
+	}
+	b := Item{
+		Name:  "armut",
+		Price: 19.5,
+	}
+	calcutePrice(a)
+	fmt.Println(a.Name, "-", a.Price, "TL", "indirimli fiyatı:", calcutePrice(a), "TL")
+	totalPrice([]Item{a})
+	fmt.Println(b.Name, b.Price, "TL")
+	fmt.Println("İndirimli toplam fiyat:", totalPrice([]Item{(a), b}), "TL")
+}
